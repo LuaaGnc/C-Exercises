bee41119a0d1c30ee915842463526c82b8e3a7d0
Luaa Gonçalves <luisgvs0401@gmail.com>
2022-11-14 18:41:00 +0000

    Update ex5.c

--- FolhaP3/Parte1/ex5.c
+++ FolhaP3/Parte1/ex5.c
@@ -13,20 +13,18 @@ void main(){
         scanf("%d", &n);
     } while (n < 2);
 
-    maiorNumero(n);
-    
+    maiorNumero(n); 
 }
 
 
 void maiorNumero(int n){
     float num, M;
-    int cont=0;
     
     for(int _ = 1; _ <= n; _++){
         printf("Digite um número real: ");
         scanf("%f", &num);
 
-        if (cont==0){M=num;cont=1;}
+        if (_==0){M=num;}
         if(num > M){M = num;}
     }
     printf("O maior número da sequência é %f", M);
