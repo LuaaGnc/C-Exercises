5bef782ab9dfc13cc63f8f2e4b89a9713f6c00e5
Luaa Gonçalves <luisgvs0401@gmail.com>
2022-11-24 14:48:13 +0000

    Vetores & Apontadores

---
+++ FolhaP4/Parte1/ex2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define Dim 10
+
+void LerVector (int[], int);
+int SomaElementosVector (int*, int);
+
+void main(){
+    int v[Dim];
+
+    LerVector(v, Dim);
+}
+
+void LerVector(int v[], int dim){
+    for(int _ = 0; _ < dim; _++)
+        scanf("%d", &v[_]);
+
+    int *p;
+    p = &v[0];
+
+    printf("A soma dos elementos do vetor é %d\n\n", SomaElementosVector(p,Dim));
+}
+
+int SomaElementosVector(int *p, int dim){
+    int soma = 0;
+    for(int _ = 0; _ < dim; _++)
+        soma+= p[_];
+
+    return soma;
+}
