0df4e9ccb2c337bc52f944052c94248c32ba60bf
Luaa Gonçalves <luisgvs0401@gmail.com>
2022-12-07 10:17:49 +0000

    trying to do by ref 12

--- FolhaP4/Parte2/ex12.c
+++ FolhaP4/Parte2/ex12.c
@@ -1,107 +1,51 @@
 #include <stdio.h>
 
-/*
-Nota que:
-Se A é uma matriz m×n e B é uma matriz n×p
-Então seu produto é uma matriz m×p
-*/
 
-/* Por isso, será necessário saber:
-Colunas da Matriz A;
-Linhas da Matriz B;
-*/
+void ReadMatrix();
+void WriteMatrix();
+void ProductMatrix();
 
-
-int NumLinhas, NumColunas, NumColunasAG, NumColunasBG;
 
 
 void main(){
-    void LerMatriz(int [][NumColunas], int , int);
-    void EscreverMatriz(int [][NumColunas], int , int);
-    void ProdutoMatriz(int [][NumColunasAG], int[][NumColunasBG], int, int, int, int);
+    int nLinhasA, nColunasA;
+    int nLinhasB, nColunasB;
+    int *nLA, *nCA;
+    int *nLB, *nCB;
+
+    nLA = &nLinhasA;
+    nCA = &nColunasA;
+    nLB = &nLinhasB;
+    nCB = &nColunasB;
+
+    printf("Insira a dimensão de A:\n");
+    printf("nº de Linhas = "); scanf("%d", nLA);
+    printf("nº de Colunas = "); scanf("%d", nCA);
+    
+    printf("Insira a dimensão de B:\n");
+    printf("nº de Linhas = "); scanf("%d", nLB);
+    printf("nº de Colunas = "); scanf("%d", nCB);
+
+    // Duas matrizes;
+    int A[*nLA][*nCA], B[*nLB][*nCB];
 
 
-    int NumLinhasA,NumColunasA, NumLinhasB, NumColunasB;
-    int A[NumLinhasA][NumColunasA], B[NumLinhasB][NumColunasB];
-
-    do{
-    printf("Insira o número de linhas da Matriz A: ");
-    scanf("%d", &NumLinhasA);
-    printf("Insira o número de colunas A: ");
-    scanf("%d", &NumColunasA);
-
-    printf("Insira o número de linhas da Matriz B: ");
-    scanf("%d", &NumLinhasB);
-    printf("Insira o número de colunas B: ");
-    scanf("%d", &NumColunasB);
-    }while(NumColunasA != NumLinhasB);
-
-    NumColunas = NumColunasA;       // Para as funções se adaptarem
-    LerMatriz(A, NumLinhasA, NumColunasA); 
-
-    NumColunas = NumColunasB;
-    LerMatriz(B, NumLinhasB, NumColunasB);
-
-    NumColunasAG = NumColunasA;
-    NumColunasBG = NumColunasB;
-
-    ProdutoMatriz(A, B, NumLinhasA, NumColunasA, NumLinhasB, NumColunasB);
 }
 
 
-void LerMatriz(int v[][NumColunas], int nL, int nC){
-    printf("Digite os números: \n");
-    for(int l = 0; l < nL; l++)
-        for(int c = 0; c < nC; c++)
-            scanf("%d", &v[l][c]);
-}
-
-void EscreverMatriz(int v[][NumColunas], int nL, int nC){
-    printf("\n\n");
-    for(int l = 0; l < nL; l++){
-        printf("|");
-        for(int c = 0; c < nC; c++)
-            printf("%d ", v[l][c]);
-        printf("|\n");
-    }
+void ReadMatrix(int *v, int *nl, int *nc){
+    *v = *nl + *nc;
+    for(int i = 0; i < *nl; i++)
+        for(int j = 0; j < *nc; j++)
+            scanf("%d", &v[i]);
 }
 
 
-/*
-Nota que:
-Se A é uma matriz  m×n  e B é uma matriz  n×p 
-Então seu produto é uma matriz m×p
-*/
+void WriteMatrix(){
 
-/*
-Pega na linha
-Multiplica pela coluna
-Adicionando cada membro
-*/
-void ProdutoMatriz(int a[][NumColunasAG], int b[][NumColunasBG], int nla, int nca, int nlb, int ncb){
-    int v[nla][ncb], z = 0;
-
-    for(int l = 0; l < nla; l++)
-        for(int c = 0; c < ncb; c++)
-            v[l][c] = 0;
-
-    // Ciclo que percorre aqueles valores 0... 1... 2... Pode ser a variável nca ou nlb, tanto faz pois elas
-    // NECESSITAM DE SER IGUAIS
-        // Ciclo que atravessa as linhas
-
-    EscreverMatriz(a, nla, nca);
-    EscreverMatriz(b, nlb, ncb);
+}
 
 
-    printf("\n\n\n");
-    for(int _ = 0; _ <= nca; _++)
-        for(int i = 0; i < nla; i++){
-            for(int k = 0; k < nca; k++){
-                /* printf("v[%d][%d] += a[%d][%d]*b[%d][%d] \n", _, i, _, k, k, i);
-                printf("%d += %d * %d \n", v[_][i], a[_][k], b[k][i]); */
-                v[_][i] += a[_][k] * b[k][i];
-            }
-        }
-        
-    EscreverMatriz(v, nla, ncb);
+void ProductMatrix(){
+
 }
