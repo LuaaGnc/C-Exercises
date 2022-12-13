0e14f7e66fa0aa04ea20523ae990aa58ae27bf95
Luaa Gonçalves <luisgvs0401@gmail.com>
2022-12-13 06:21:16 +0000

    ex 6 done

---
+++ FolhaP5/Parte1/ex6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+#define DIM 70
+
+void main(){
+    char frase[DIM];
+    char *conta;
+    conta = &frase[0];
+    
+    printf("Insira seu nome: ");
+    gets(frase);
+
+    printf("O número de dígitos na frase inserida é %d\n\n", contadigitos(conta));
+}
+
+
+int contadigitos(char *s){
+    int contador = 0;
+    for(int k = 0; k < strlen(s); k++)
+        if(*(s+k) >= '0' && *(s+k) <= '9')
+            contador++;
+    return contador;
+}
